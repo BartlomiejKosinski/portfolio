daef0230ae6401d365f0d5c76c92f34755ef3f7e
Bartlomiej <bartekkosinski07@gmail.com>
2023-05-16 21:41:49 +0200

    mobile bugfix

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let menu = 0;
+let menu = 1;
 
 const hamburger = document.getElementById("bi");
 const nav = document.getElementById("nav");
 hamburger.addEventListener("click", () => {
     if(menu == 0)
     {
-        nav.style.display = "block";
         menu = 1;
-    }else{
-        nav.style.display = "none";
+        nav.classList.remove("block"); 
+    }else if(menu == 1){
         menu = 0;
+        nav.classList.add("block");
     }
 })
